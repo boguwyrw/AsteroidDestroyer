7386e6706320594dac95426e6505fac01e054f9c
boguwyrw <b.wyrwal@wp.pl>
2020-09-18 22:08:17 +0200

    First sketch of the game

---
+++ AsteroidDestroyer/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserScript : MonoBehaviour
+{
+    private Rigidbody2D rigidbody2D;
+    private float laserSpeed = 40;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.AddForce(transform.up * laserSpeed);
+    }
+}
---
+++ AsteroidDestroyer/Assets/Scripts/SpaceshipScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipScript : MonoBehaviour
+{
+    private Vector2 screenBounds;
+    private float spaceshipSpeed = 8;
+
+    private void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+    }
+
+    private void Update()
+    {
+        PlayerMovement();
+        PlayerBounds();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(transform.right * spaceshipSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-transform.right * spaceshipSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(transform.up * spaceshipSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(-transform.up * spaceshipSpeed * Time.deltaTime);
+        }
+    }
+
+    private void PlayerBounds()
+    {
+        Vector2 playerPosition = transform.position;
+        playerPosition.x = Mathf.Clamp(transform.position.x, -screenBounds.x + 1.5f, screenBounds.x - 1.5f);
+        playerPosition.y = Mathf.Clamp(transform.position.y, -screenBounds.y, screenBounds.y);
+        transform.position = playerPosition;
+    }
+}
