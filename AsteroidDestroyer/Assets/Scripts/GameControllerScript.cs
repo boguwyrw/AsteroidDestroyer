f3ee593613d40cf7499e76e5d106bd9e201177d9
boguwyrw <b.wyrwal@wp.pl>
2020-09-25 13:52:05 +0200

    Game Controller was added

---
+++ AsteroidDestroyer/Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControllerScript : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
